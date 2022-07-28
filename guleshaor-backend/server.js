aa27ef90da099ab4da2e0ae7b2e89d49cb7c0b8f
sibteali786 <sibteali786@gmail.com>
2022-07-28 09:28:50 +0500

    made route changes

--- guleshaor-backend/server.js
+++ guleshaor-backend/server.js
@@ -5,6 +5,7 @@ import mentorRoutes from "./routes/mentorRoutes.js"
 import studentRoutes from "./routes/studentRoutes.js"
 import studentsMentorRoutes from "./routes/studentsMentorRoutes.js"
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import path from 'path';
 // for environment variables 
 dotenv.config()
 
@@ -13,18 +14,21 @@ connectDB()
 // Initialize the express
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Api is running")
-})
+
 //using productRoutes and userRoutes
 app.use("/api/mentors",mentorRoutes)
 app.use("/api/students",studentRoutes)
 app.use("/api/getAllStudents",studentsMentorRoutes)
 app.use(notFound)
 app.use(errorHandler)
-
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('guleshaor-frontend/build'))
+    app.use(express.static(path.join(__dirname,'guleshaor-frontend/build')))
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'guleshaor-frontend','build','index.html')))
+}else{
+    app.get('/',(req,res)=>{
+        res.send("Api is running")
+    })
 }
 
 const PORT = process.env.PORT || 5000
