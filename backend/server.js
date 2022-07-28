1034d02aa0cf586be13835d840e922c551427ee5
sibteali786 <sibteali786@gmail.com>
2022-07-28 12:26:52 +0500

    removed errors

--- backend/server.js
+++ backend/server.js
@@ -14,20 +14,6 @@ dotenv.config()
 connectDB()
 // Initialize the express
 const app = express();
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('guleshaor-frontend/build'))
-}
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname,'frontend/build')))
-
-  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-}else{
-  app.get("/", (req, res) => {
-    res.send("Api is Running......");
-  });
-}
 //using productRoutes and userRoutes
 app.use("/api/mentors",mentorRoutes)
 app.use("/api/students",studentRoutes)
@@ -36,5 +22,15 @@ app.use(notFound)
 app.use(errorHandler)
 
 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'/frontend/build')))
+
+  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+  app.get("/", (req, res) => {
+    res.send("Api is Running......");
+  });
+}
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
