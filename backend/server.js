564141fa35541d70b3b905de91671920a12750a9
sibteali786 <sibteali786@gmail.com>
2022-07-28 11:20:35 +0500

    chnaged folder names

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express  from "express";
+import path from 'path';
 import dotenv from "dotenv"
+import colors from 'colors';
 import connectDB from "./config/db.js";
 import mentorRoutes from "./routes/mentorRoutes.js"
 import studentRoutes from "./routes/studentRoutes.js"
@@ -13,9 +15,19 @@ connectDB()
 // Initialize the express
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Api is running")
-})
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('guleshaor-frontend/build'))
+}
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'frontend/build')))
+
+  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+  app.get("/", (req, res) => {
+    res.send("Api is Running......");
+  });
+}
 //using productRoutes and userRoutes
 app.use("/api/mentors",mentorRoutes)
 app.use("/api/students",studentRoutes)
@@ -23,9 +35,6 @@ app.use("/api/getAllStudents",studentsMentorRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('guleshaor-frontend/build'))
-}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
