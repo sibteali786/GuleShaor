7402401c6cfccb22903b703f68c6c729aa4c3913
sibteali786 <sibteali786@gmail.com>
2022-07-29 14:09:35 +0500

    made changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,19 @@ const app = express();
 app.use("/api/mentors",mentorRoutes)
 app.use("/api/students",studentRoutes)
 app.use("/api/getAllStudents",studentsMentorRoutes)
-app.use(notFound)
-app.use(errorHandler)
 
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'/frontend/build')))
-
-  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+  
+  app.get('/*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
 }else{
   app.get("/", (req, res) => {
     res.send("Api is Running......");
   });
 }
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
