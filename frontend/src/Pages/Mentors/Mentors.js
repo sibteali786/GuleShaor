a54f540e956e4c1b9664f468817bee30c4eed5a3
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-08-10 15:31:02 +0500

    made some style changes to mentor and student

--- frontend/src/Pages/Mentors/Mentors.js
+++ frontend/src/Pages/Mentors/Mentors.js
@@ -1,33 +1,39 @@
-import { Grid } from '@mui/material'
-import React, {useEffect} from 'react'
-import "./Mentors.scss"
-import Users from '../../components/Users/Users'
+import { Grid } from "@mui/material";
+import React, { useEffect } from "react";
+import "./Mentors.scss";
+import Users from "../../components/Users/Users";
 import { useDispatch, useSelector } from "react-redux";
-import Message from '../../components/Message/Message'
-import {listMentors} from "../../actions/mentorActions";
-import Loader from '../../components/Loader/Loader';
+import Message from "../../components/Message/Message";
+import { listMentors } from "../../actions/mentorActions";
+import Loader from "../../components/Loader/Loader";
 const Mentors = () => {
-  
-    const dispatch = useDispatch();
-    const mentorList = useSelector(state => state.mentorList)
-    const {loading, error, mentors} = mentorList;
-    useEffect(() => {
-      dispatch(listMentors());
-    }, [dispatch])
+  const dispatch = useDispatch();
+  const mentorList = useSelector((state) => state.mentorList);
+  const { loading, error, mentors } = mentorList;
+  useEffect(() => {
+    dispatch(listMentors());
+  }, [dispatch]);
   return (
     <>
-    {loading ? <Loader/> : error ? <Message>{error}</Message> : (
-      
-      <Grid container spacing={2} style={{padding:"6rem 2rem"}} alignItems="center" justifyContent="space-evenly">
-      {mentors.map((mentor)=>(
-        <Grid key={mentor._id} item sm={6} md={4} lg={3}>
-        <Users mentor={mentor} />
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          style={{ padding: "4rem 2rem", marginTop: "0" }}
+        >
+          {mentors.map((mentor) => (
+            <Grid key={mentor._id} item sm={6} md={4} lg={3}>
+              <Users mentor={mentor} />
+            </Grid>
+          ))}
         </Grid>
-        ))}
-        </Grid>
-        ) }
-        </>
-  )
-}
+      )}
+    </>
+  );
+};
 
-export default Mentors
+export default Mentors;
--- frontend/src/Pages/Students/Students.js
+++ frontend/src/Pages/Students/Students.js
@@ -1,31 +1,38 @@
-import { Grid } from '@mui/material'
-import React, {useEffect} from 'react'
-import Users from '../../components/Users/Users'
+import { Grid } from "@mui/material";
+import React, { useEffect } from "react";
+import Users from "../../components/Users/Users";
 import { useDispatch, useSelector } from "react-redux";
-import Message from '../../components/Message/Message'
-import Loader from '../../components/Loader/Loader';
-import { listStudents } from '../../actions/studentActions';
+import Message from "../../components/Message/Message";
+import Loader from "../../components/Loader/Loader";
+import { listStudents } from "../../actions/studentActions";
 const Students = () => {
-    const dispatch = useDispatch();
-    const studentList = useSelector(state => state.studentList)
-    const {loading, error, students} = studentList;
-    useEffect(() => {
-      dispatch(listStudents());
-    }, [dispatch])
+  const dispatch = useDispatch();
+  const studentList = useSelector((state) => state.studentList);
+  const { loading, error, students } = studentList;
+  useEffect(() => {
+    dispatch(listStudents());
+  }, [dispatch]);
   return (
     <>
-    {loading ? <Loader/> : error ? <Message>{error}</Message> : (
-      
-      <Grid container spacing={2} style={{padding:"6rem 2rem"}} alignItems="center" justifyContent="space-evenly">
-      {students.map((student)=>(
-        <Grid key={student._id} item sm={6} md={4} lg={3}>
-        <Users mentor={student} />
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          style={{ padding: "4rem 2rem", marginTop: "0" }}
+        >
+          {students.map((student) => (
+            <Grid key={student._id} item sm={6} md={4} lg={3}>
+              <Users mentor={student} />
+            </Grid>
+          ))}
         </Grid>
-        ))}
-        </Grid>
-        ) }
-        </>
-  )
-}
+      )}
+    </>
+  );
+};
 
-export default Students
+export default Students;
